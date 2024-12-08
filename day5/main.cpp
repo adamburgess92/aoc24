e56d00e8b2bea3462cd6a05565ce6bdcbbad17ce
Adam Burgess <adam.burgess92@gmail.com>
2024-12-08 05:45:06 +0000

    Day 5 part 1 progress

--- day5/main.cpp
+++ day5/main.cpp
@@ -34,36 +34,69 @@ vector<vector<int>> parse_updates(vector<string>& vs) {
     return v_out;
 }
 
-multimap<int, int> parse_rules(vector<string>& vs) {
+multimap<int, int> parse_rules(vector<string> vs) {
     multimap<int, int> map_out; 
     for (int i=0; i<vs.size(); ++i) {
-        string left = vs[i].substr(vs[i].begin(), vs[i].find('|'));
-        string right = vs[i].substr(vs[i].find('|'), vs[i].begin());
+        string left = vs[i].substr(0, vs[i].find('|'));
+        string right = vs[i].substr(vs[i].find('|')+1);
         int left_int = stoi(left);
         int right_int = stoi(right);
-        multimap.insert(pair{left_int, right_int});
+        map_out.insert(pair{left_int, right_int});
     }
     return map_out;
 
 }
 
-void part1(vector<vector<int>> updates) {
+void part1(vector<vector<int>> updates, multimap<int, int> rules) {
     for (int i=0; i<updates.size(); ++i) {
         for (int j=0; j<updates[i].size(); ++j) {
+            bool order_correct = true;
             vector<int> before(updates[i].begin(), updates[i].begin()+j);
-            vector<int> after(updates[i].begin()+j, updates[i].end());
-            int val = after[0];
-            cout << before.size() << endl;
-            cout << after.size() << endl;
+            vector<int> after(updates[i].begin()+j+1, updates[i].end());
+            int target = updates[i][j];
+            cout << "target: " << target << endl;
+            // Get rules before
+            vector<int> before_rules; 
+            vector<int> after_rules;
+            for (const auto& [k, v] : rules) {
+                if (k==target) {
+                    after_rules.push_back(v);
+                }
+                if (v==target) {
+                    before_rules.push_back(k);
+                }
+            }
+            // Ensure that everything in 'before' appears int 'before_rules': 
+            bool before_ok = true;
+            if (before.size()>0) {
+                before_ok = all_of(before.begin(), before.end(), [&](int e) {
+                    return find(before_rules.begin(), before_rules.end(), e) != before_rules.end();
+                });
+                cout << "before ok: " << before_ok << endl;
+            }
+            // And again for 'after'
+            bool after_ok = true;
+            if (after.size()>0) {
+                after_ok = all_of(after.begin(), after.end(), [&](int e) {
+                    return find(after_rules.begin(), after_rules.end(), e) != after_rules.end();
+                });
+                cout << "after ok: " << after_ok << endl;
+            }
+            if (!before_ok || !after_ok) {
+                order_correct = false;
+            }
+            cout << "order_correct: " << order_correct << endl;
         }
         cout << endl;
     }
 }
 
 int main() {
-    vector<string> d = parse_data("test_data.txt");
-    vector<vector<int>> updates = parse_updates(d);
-    multimap<int, int> rules = parse_rules(d);
-    // part1(updates);
+    vector<string> update_data = parse_data("test_update.txt");
+    vector<vector<int>> updates = parse_updates(update_data);
 
+    vector<string> rule_data = parse_data("test_rules.txt");
+    multimap<int, int> rules = parse_rules(rule_data);
+
+    part1(updates, rules);
 }
