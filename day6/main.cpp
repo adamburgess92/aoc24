59dba9b661693608638a0f81c56b3bfa6975b7e5
Adam Burgess <adam.burgess92@gmail.com>
2024-12-11 16:59:26 +1100

    Day 6 Part 2 solved

--- day6/main.cpp
+++ day6/main.cpp
@@ -1,19 +1,21 @@
-#include <iostream> 
-#include <fstream> 
-#include <string> 
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include <functional>
 #include <stdexcept>
 #include <utility>
 #include <set>
+#include <map>
+#include <tuple>
 
 using namespace std;
 
 vector<vector<char>> parse_data(const string& filename) {
     ifstream inputFile(filename);
     string line;
-    vector<vector<char>> v_out; 
+    vector<vector<char>> v_out;
     while (getline(inputFile, line)) {
         vector<char> v;
         for (int i=0; i<line.length(); ++i) {
@@ -25,145 +27,155 @@ vector<vector<char>> parse_data(const string& filename) {
     return v_out;
 }
 
-class Guard {
-public: 
-    int row;
-    int col;
-    char dir = 'n';
-    vector<pair<int, int>> visited;
-    Guard(int row, int col) : row(row), col(col) {};
-    void logPosition(int row, int col) {
-        visited.push_back({row, col});
-    }
-    void turn() {
-        switch (dir) {
-            case 'n' :
-                dir = 'e';
-                break;
-            case 'e' :
-                dir = 's';
-                break;
-            case 's' : 
-                dir = 'w';
-                break;
-            case 'w' : 
-                dir = 'n';
-                break;
-        }
-    }
-    void step() {
-        switch (dir) {
-            case 'n' :
-                row -= 1;
-                break;
-            case 'e' :
-                col += 1;
-                break;
-            case 's' : 
-                row += 1;
-                break;
-            case 'w' : 
-                col -= 1;
-                break;
-        }
-    }
-};
+vector<tuple<int, int>> getPath(vector<vector<char>> grid) {
+    int min_i = 0;
+    int min_j = 0;
+    int max_i = grid.size()-1;
+    int max_j = grid[0].size()-1;
 
-Guard findStart(vector<vector<char>> grid) {
+    // Find start:
+    int init_i;
+    int init_j;
     for (int i=0; i<grid.size(); ++i) {
-        for (int j=0; j<grid[i].size(); ++j) {
-            if (grid[i][j]=='^') {
-                return Guard(i, j);
+        for (int j=0; j<grid[0].size(); ++j) {
+            if (grid[i][j] == '^') {
+                init_i = i;
+                init_j = j;
             }
         }
     }
-}
 
-char getNext(vector<vector<char>> grid, Guard currentLoc) {
-    try {
-        switch (currentLoc.dir) {
-            case 'n':
-                return grid.at(currentLoc.row-1).at(currentLoc.col);
-            case 'e':
-                return grid.at(currentLoc.row).at(currentLoc.col+1);
-            case 's' :
-                return grid.at(currentLoc.row+1).at(currentLoc.col);
-            case 'w':
-                return grid.at(currentLoc.row).at(currentLoc.col-1);
+    // Init stuff
+    int curr_i = init_i;
+    int curr_j = init_j;
+    bool inbounds = true;
+    map<char, tuple<int,int>> d_map{{'n', tuple(-1, 0)}, {'e', tuple(0, 1)}, {'s', tuple(1, 0)}, {'w', tuple(0, -1)}};
+    char d = 'n';
+
+    // Keep a log of visited locations:
+    vector<tuple<int, int>> v;
+
+    while (inbounds) {
+        v.push_back(make_tuple(curr_i, curr_j));
+        int dx = get<0>(d_map[d]);
+        int dy = get<1>(d_map[d]);
+        if (curr_i+dx>max_i || curr_j+dy>max_j || curr_i+dx<0 || curr_j+dy<0) {
+            inbounds = false;
+            break;
         }
-    } catch (const out_of_range& e) {
-        return 'f';
+        char next_c = grid[curr_i+dx][curr_j+dy];
+        if (next_c == '#') {
+            switch (d) {
+                case 'n' :
+                    d = 'e';
+                    break;
+                case 'e' :
+                    d = 's';
+                    break;
+                case 's' :
+                    d = 'w';
+                    break;
+                case 'w' :
+                    d = 'n';
+                    break;
+            }
+            continue;
+        }
+        curr_i += dx;
+        curr_j += dy;
     }
+    return v;
 }
-
 
 int part1(vector<vector<char>> grid) {
-    Guard guard = findStart(grid);
-    char next = getNext(grid, guard);
-    while (next != 'f') {
-        // cout << "Guard pos: " << guard.row << ", " << guard.col << endl;
-        guard.logPosition(guard.row, guard.col);
-        if (next != '#') {
-            guard.step();
-        } else {
-            guard.turn();
-            guard.step();
-        }
-        next = getNext(grid, guard);
-    }
-    // return s;
-    set<pair<int, int>> unique_pairs(guard.visited.begin(), guard.visited.end());
-    return unique_pairs.size()+1; // Not sure why off-by-one
+    vector<tuple<int, int>> p = getPath(grid);
+    // for (tuple<int, int> t : p) {
+    //     cout << get<0>(t) << ", " << get<1>(t) << endl;
+    // }
+    return set(p.begin(), p.end()).size();
 }
 
 int part2(vector<vector<char>> grid) {
-    // Guard guard = findStart(grid);
-    int n = 0;
-    for (int row=0; row<grid.size(); ++row) {
-        for (int col=0; col<grid[0].size(); ++col) {
-            Guard guard = findStart(grid);
-            if (row==guard.row && col==guard.col) {
+    int min_i = 0;
+    int min_j = 0;
+    int max_i = grid.size()-1;
+    int max_j = grid[0].size()-1;
+    // Get guard path
+    vector<tuple<int, int>> path = getPath(grid);
+    set<tuple<int, int>> u_path(path.begin(), path.end());
+    int n=0;
+    int n_total = u_path.size();
+    // Find start:
+    int init_i;
+    int init_j;
+    for (int i=0; i<grid.size(); ++i) {
+        for (int j=0; j<grid[0].size(); ++j) {
+            if (grid[i][j] == '^') {
+                init_i = i;
+                init_j = j;
+            }
+        }
+    }
+    for (tuple<int, int> point : u_path) {
+        int r = get<0>(point);
+        int c = get<1>(point);
+        if (r==init_i && c==init_j) {
+            continue;
+        }
+        // Pretty much do the same thing as part 1 again, with a blockade...
+        int curr_i = init_i;
+        int curr_j = init_j;
+        bool inbounds = true;
+        map<char, tuple<int,int>> d_map{{'n', tuple(-1, 0)}, {'e', tuple(0, 1)}, {'s', tuple(1, 0)}, {'w', tuple(0, -1)}};
+        char d = 'n';
+        // Keep a log of visited locations:
+        vector<tuple<int, int>> v;
+        // Loop
+        while (inbounds) {
+            tuple<int, int> curr_tpl = make_tuple(curr_i, curr_j);
+            v.push_back(curr_tpl);
+            int dx = get<0>(d_map[d]);
+            int dy = get<1>(d_map[d]);
+            if (curr_i+dx>max_i || curr_j+dy>max_j || curr_i+dx<0 || curr_j+dy<0) {
+                inbounds = false;
+                break;
+            }
+            char next_c = grid[curr_i+dx][curr_j+dy];
+            if (next_c == '#' || (curr_i+dx==r && curr_j+dy==c)) {
+                switch (d) {
+                    case 'n' :
+                        d = 'e';
+                        break;
+                    case 'e' :
+                        d = 's';
+                        break;
+                    case 's' :
+                        d = 'w';
+                        break;
+                    case 'w' :
+                        d = 'n';
+                        break;
+                }
                 continue;
             }
-            // Place blockade:
-            char was = grid[row][col];
-            grid[row][col] = '#';
-            cout << "Blockade at: " << row << ", " << col << endl;
-            bool inLoop = false;
-            // Start moving guard
-            char next = getNext(grid, guard);
-            // cout << next << endl;
-            while (next!='f') {
-                // cout << guard.row << ", " << guard.col << endl;
-                guard.logPosition(guard.row, guard.col);
-                if (next != '#') {
-                    guard.step();
-                } else {
-                    guard.turn();
-                    guard.step();
-                }
-                next = getNext(grid, guard);
-                // Check num times visited point if have visited 
-                int n_times_visit = count(guard.visited.begin(), guard.visited.end(), pair{guard.row, guard.col});
-                // cout << n_times_visit << endl;
-                if (n_times_visit>4) {
-                    inLoop = true;
-                    break;
-                }
+            if (v.size()>10000) {
+                n +=1;
+                inbounds = false; // to break out of outer loop
+                cout << curr_i << ", " << curr_j << " | " << n << "/" << n_total << endl;
+                break;
             }
-            if (inLoop) {
-                n+=1;
-            }
-            // Reset blockade: 
-            grid[row][col] = was;
+            curr_i += dx;
+            curr_j += dy;
         }
     }
     return n;
 }
 
 int main() {
-    vector<vector<char>> grid = parse_data("data.txt");
-    // int res1 = part1(grid);
-    // cout << res1 << endl;
-    cout << part2(grid) << endl;
+    vector<vector<char>> d = parse_data("data.txt");
+    int res1 = part1(d);
+    cout << res1 << endl;
+
+    int res2 = part2(d);
+    cout << res2 << endl;
 }
