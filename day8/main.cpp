621bb69fce9d07d4b6281ef4172805fec45cde97
Adam Burgess <adam.burgess92@gmail.com>
2024-12-12 13:05:38 +0000

    Day 8 solved

--- day8/main.cpp
+++ day8/main.cpp
@@ -37,13 +37,13 @@ map<char, vector<Point>> get_points(vector<string>& grid) {
         for (int col=0; col<grid[row].size(); ++col) {
             char c = grid[row][col];
             if (c!='.') {
-                // If key doesn't exist yet:
                 bool key_exists = false;
                 for (auto [key, val] : m) {
                     if (key==c) {
                         key_exists = true;
                     }
                 }
+                // If new key:
                 if (!key_exists) {
                     Point new_point = Point(row, col);
                     m[c] = vector{new_point};
@@ -87,15 +87,52 @@ void part1(vector<string> grid) {
         }
     }
     // Get distinct antinodes: 
+    // cout << antinodes.size() << endl; 
+    set<Point> unique_antinodes(antinodes.begin(), antinodes.end());
+    cout << unique_antinodes.size() << endl;
+}
+
+void part2(vector<string> grid) {
+    // Declare boundaries: 
+    int min_row = 0;
+    int min_col = 0;
+    int max_row = grid.size()-1;
+    int max_col = grid[0].size()-1;
+    // Instantiate anotnodes
+    vector<Point> antinodes;
+    // Build map
+    map<char, vector<Point>> points_map = get_points(grid);
+    for (auto [k, v] : points_map) {
+        // cout << k << endl;
+        for (Point p: v) {
+        // Get dx, dy to other points
+            for (Point q : v) {
+                int dx = q.col - p.col;
+                int dy = q.row - p.row;
+                // Find antinodes around point p
+                if (dx==0 && dy==0) {
+                    continue; // Probably unecessary
+                }
+                int r = q.row;
+                int c = q.col;
+                // Antinode location:
+                while (r>=min_row && r<=max_row && c>=min_col && c<=max_col) {
+                    antinodes.push_back(Point(r, c));
+                    // cout << r << ", " << c << endl;
+                    r += dy;
+                    c += dx;
+                } 
+            }
+        }
+    }
+    // Get distinct antinodes:
+    // cout << antinodes.size() << endl; 
     set<Point> unique_antinodes(antinodes.begin(), antinodes.end());
     cout << unique_antinodes.size() << endl;
 }
 
 int main() {
     vector<string> d = parse_data("data.txt");
-    for (const string& s : d) {
-        cout << s << endl;
-    }
-    cout << endl;
-    part1(d);
+    // part1(d);
+    part2(d);
 }
